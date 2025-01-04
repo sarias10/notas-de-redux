d1d100b02f12626437af94e3bfd36a97d01d7c81
Sergio Arias <s-andres140@hotmail.com>
2025-01-04 06:42:55 -0500

    feat: :hammer: add noteActionCreator for simplify app logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import store from './reducers/noteReducer.js'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
+
--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -20,37 +20,6 @@ const noteReducer = (state = [], action) => {
   }
 }
   
-  const store = createStore(noteReducer)
-  
-  store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-      content: 'the app state is in redux store',
-      important: true,
-      id: 1
-    }
-  })
-  
-  store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-      content: 'state changes are made with actions',
-      important: false,
-      id: 2
-    }
-  })
-  
-  const App = () => {
-    return(
-      <div>
-        <ul>
-          {store.getState().map(note=>
-            <li key={note.id}>
-              {note.content} <strong>{note.important ? 'important' : ''}</strong>
-            </li>
-          )}
-          </ul>
-      </div>
-    )
-  }
-  export default noteReducer;
+const store = createStore(noteReducer)
+
+export default store;
