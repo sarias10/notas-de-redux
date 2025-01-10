e846479bf553f597c04cb2585cfdab7ab4709e89
Sergio Arias <s-andres140@hotmail.com>
2025-01-10 07:05:20 -0500

    refactor: :recycle: refactor noteReducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,18 @@
 import ReactDOM from 'react-dom/client'
 import { createStore, combineReducers } from "redux"
 import { Provider } from 'react-redux'
-
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App.jsx'
 
 import noteReducer, { createNote } from './reducers/noteReducer'
 import filterReducer, { filterChange } from './reducers/filterReducer.js'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
-
-store.subscribe(()=> console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 console.log(store.getState())
 
